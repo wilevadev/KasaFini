0f1a7f9f508db18d96efcbcc5a6926931de1d15a
vanessa duval <vanessadevetudiante@gmail.com>
2023-03-24 14:59:54 +0100

    page error

--- src/Pages/Housing/Housing.jsx
+++ src/Pages/Housing/Housing.jsx
@@ -1,38 +1,29 @@
 import React, { useEffect, useState } from 'react';
-import { useParams} from 'react-router-dom';
-
-// Import des composants
+import { useNavigate, useParams } from 'react-router-dom';
 
 import Collapse from '../../components/Collapse/Collapse';
 import Carousel from '../../components/Carousel/Carousel';
 import StarRating from '../../components/Stars/Stars';
 import Tag from '../../components/Tags/Tags';
-// Import du fichier CSS
 import './Housing.css';
 
-// Création de la page Housing
-const Housing = ({history}) => {
-  // Utilisation de useState pour gérer les données et les données du logement
-  const [data, setData] = useState([]);
+
+const Housing = () => {
   const { id } = useParams();
   const [HousingData, setHousingData] = useState({});
+  const navigate = useNavigate();
 
-  // Charger les données JSON au montage du composant
   useEffect(() => {
     fetch('/Logements.json')
       .then((res) => res.json())
-      .then((data) => setData(data))
-      .catch(() => history.push('/error'));
-  }, [history]);
+      .then((data) => {
+        const housing = data.find((item) => item.id === id);
+        setHousingData(housing);
+        if (!housing) navigate('/404');
+      })
+      .catch(() => navigate('/404'));
+  }, [id, navigate]);
 
-  // Mettre à jour les données du logement lorsque "data" ou "id" changent
-  useEffect(() => {
-    if (data && data.length > 0) {
-      setHousingData(data.find((housing) => housing.id === id));
-    }
-  }, [data, id]);
-
-  // Destructuration des propriétés de HousingData
   const {
     title,
     pictures,
@@ -44,78 +35,67 @@ const Housing = ({history}) => {
     equipments,
   } = HousingData;
 
-  // Rendu de la page Housing
   return (
     <div>
-      {/* Vérifie si HousingData n'est pas vide */}
-        {HousingData && Object.keys(HousingData).length !== 0 ? (
-          <div className="logement">
-            <div className="logement-details">
-              {/* Carousel d'images */}
-              <Carousel images={pictures} />
-              <div className="retail">
-                <div className="left-column">
-                  {/* Titre du logement */}
-                  <h1 className="single-line-text">{title}</h1>
-                  {/* Localisation du logement */}
-                  <p className="locations">{location}</p>
-                  {/* Tags */}
-                  <div className="tags-container">
-                    {tags.map((tag) => (
-                      <Tag key={tag} text={tag} />
-                    ))}
-                  </div>
-                </div>
-                <div className="right-column">
-                  {/* Informations sur l'hôte */}
-                  <div className="host-info">
-                    <p className="host-name">{host.name}</p>
-                    {host.picture && (
-                      <img
-                        src={host.picture}
-                        alt={host.name}
-                        className="host-image"
-                      />
-                    )}
-                  </div>
-                  {/* Évaluation du logement */}
-                  <div className="stars">
-                    <StarRating rating={rating} />
-                  </div>
+      {HousingData && Object.keys(HousingData).length !== 0 ? (
+        <div className="logement">
+          <div className="logement-details">
+            <Carousel images={pictures} />
+            <div className="retail">
+              <div className="left-column">
+                <h1 className="single-line-text">{title}</h1>
+                <p className="locations">{location}</p>
+                <div className="tags-container">
+                  {tags.map((tag) => (
+                    <Tag key={tag} text={tag} />
+                  ))}
                 </div>
               </div>
-              <div className="Collapse1">
-                <div className="half-width description">
-                  {/* Description du logement */}
-                  <Collapse
-                    id="description"
-                    title={<span className='collapse-title'>Description</span>}
-                    content={
-                      <div className="description-content">{description}</div>
-                    }
-                  />
-                </div>
-                <div className="half-width equipments">
-                  {/* Liste des équipements */}
-                  <Collapse
-                    id="equipments"
-                    title={<span className='collapse-title'>Equipements</span>}
-                    content={
-                      <ul className="comma-list">
-                        {equipments.map((equipment) => (
-                          <li key={equipment}>{equipment} </li>
-                          ))}
-                        </ul>
-                      }
+              <div className="right-column">
+                <div className="host-info">
+                  <p className="host-name">{host.name}</p>
+                  {host.picture && (
+                    <img
+                      src={host.picture}
+                      alt={host.name}
+                      className="host-image"
                     />
-                  </div>
+                  )}
+                </div>
+                <div className="stars">
+                  <StarRating rating={rating} />
                 </div>
               </div>
             </div>
-          ) : null}
+            <div className="Collapse1">
+              <div className="half-width description">
+                <Collapse
+                  id="description"
+                  title={<span className='collapse-title'>Description</span>}
+                  content={
+                    <div className="description-content">{description}</div>
+                  }
+                />
+              </div>
+              <div className="half-width equipments">
+                <Collapse
+                  id="equipments"
+                  title={<span className='collapse-title'>Equipements</span>}
+                  content={
+                    <ul className="comma-list">
+                      {equipments.map((equipment) => (
+                        <li key={equipment}>{equipment} </li>
+                      ))}
+                    </ul>
+                  }
+                />
+              </div>
+            </div>
+          </div>
         </div>
-    );
-  };
-  
-  // Export du composant Housing
-  export default Housing;
+      ) : null}
+    </div>
+  );
+};
+
+export default Housing;
